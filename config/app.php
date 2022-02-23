637c56f32e0b76bc9897f48778aa081b66c3f09e
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-02-23 12:23:41 -0600

    Cambiada url a la que apunta la documentacion de swagger

--- config/app.php
+++ config/app.php
@@ -56,7 +56,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'urlApi' => env('APP_URL_API', 'http://127.0.0.1:8000/api'),
+    'urlApi' => env('APP_URL_API', 'http://localhost/api'),
 
     'asset_url' => env('ASSET_URL', null),
 
